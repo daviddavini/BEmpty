60c2a84f7411589d65f184b6fca7c27e256f89ed
FJXEF <eric42fang@gmail.com>
2022-04-19 15:43:30 -0700

    updated landing page
    created background and basic content for landing page, experimented on CSS styling

---
+++ bempty/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+        <h1 classname = "blah">BEmpty</h1>
+        <a>find your next study place <br/></a>
+        <button>North Campus</button>
+        <button>South Campus</button>
+    </div>
+  )
+}
+
+export default Home
