d78f72688dd083cabc9fda09ba4890e77ce43d02
FJXEF <eric42fang@gmail.com>
2022-04-20 21:38:16 -0700

    changed font color
    changed font color of text in the campus component, just a trivial update.

--- bempty/src/Campus.js
+++ bempty/src/Campus.js
@@ -4,8 +4,8 @@ function Campus() {
 
   return (
     <div>
-        <div>Welcome to the South Campus!</div>
-        <div>Search Buildings: </div>
+        <div class="welcome">Welcome to the South Campus!</div>
+        <div class="welcome">Search Buildings: </div>
         <div class="building">
             <a>Boelter Hall</a>
             <a>Empty classrooms: 53</a>
