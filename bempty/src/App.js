b5948c416d0f2e6819529189237ca8f208dc1283
FJXEF <eric42fang@gmail.com>
2022-04-20 20:10:44 -0700

    Added Navbar
    Added Basic Navbar to front page

--- bempty/src/App.js
+++ bempty/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
 import Home from "./Home";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<><Home/></>} />
+          <Route path="/" element={<><Header/><Home/></>} />
         </Routes>
       </Router>
     </div>
---
+++ bempty/src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.css";
+
+
+
+function Header() {
+  return (
+    <div className="header">
+        <div class="header__logo">BEMPTY</div>
+        
+        {/*<div className='header__search'>
+            <input className='header__searchInput' type="text"></input>
+        </div>*/}
+
+    </div>
+  )
+}
+
+export default Header
