bcbd46a729188b046f98b90a9f26930bd81e5485
FJXEF <eric42fang@gmail.com>
2022-05-05 18:29:08 -0700

    restrucutred frontend
    moved components into a component folder

--- bempty/src/App.js
+++ bempty/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './App.css';
-import Header from './Header';
-import Home from "./Home";
-import Campus from "./Campus";
+import Header from './components/Header';
+import Home from "./components/Home";
+import Campus from "./components/Campus";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
