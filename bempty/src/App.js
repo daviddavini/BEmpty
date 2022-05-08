b40e6f96ebc954e5ccf9ba5f808a0474c9378f8b
FJXEF <eric42fang@gmail.com>
2022-05-08 14:47:11 -0700

    added classroom component
    as title

--- bempty/src/App.js
+++ bempty/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from "./components/Home";
 import Campus from "./components/Campus";
+import Classrooms from "./components/Classrooms"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Header/>} >
-            <Route index element={<Home/>} />
-            <Route path="/Campus/:location" element={<Campus/>} />
+                <Route index element={<Home/>} />
+                <Route path="/Campus/:location" element={<Campus/>} />
+                <Route path="/Classrooms" element={<Classrooms/>} />
           </Route>
         </Routes>
       </Router>
---
+++ bempty/src/components/Classrooms.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Classrooms.css'
+
+function Classrooms() {
+  return (
+    <div class="classrooms_panel">
+        <div class="welcome">Welcome to Boelter Hall!</div>
+        <div class="welcome">Empty Classrooms: </div>
+        <div class="classroom">
+            <a>6227</a>
+        </div>
+        <div class="classroom">
+            <a>6228</a>
+        </div>
+        <div class="classroom">
+            <a>6229</a>
+        </div>
+    </div>
+  )
+}
+
+export default Classrooms
